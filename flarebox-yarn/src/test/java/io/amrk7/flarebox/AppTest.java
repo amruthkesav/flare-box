34397d2d329b240d6aff82cb7da20ac7f2d7292c
amruths <amruths@uber.com>
2024-08-26 19:19:23 +0530

    Initial commit, bootstrap parent and flarebox-yarn modules

---
+++ flarebox-yarn/src/test/java/io/amrk7/flarebox/AppTest.java
@@ -0,0 +1,20 @@
+package io.amrk7.flarebox;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
